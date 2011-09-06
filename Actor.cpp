41c96d7511ec6c0b8ae7551b3c3084336e04b418
Chris Frisz <cjfrisz@chrisfrisz.com>
2011-09-06 14:27:28 +0000

    Delegating Actor behaviors to subclasses.

--- Actor.cpp
+++ Actor.cpp
@@ -28,67 +28,13 @@ namespace evolver {
     return *this;
   }
 
-  float Actor::getMoveSpeed () {
-    return this->moveSpeed;
-  }
-
   Coordinates<float> *Actor::getOrigin () {
     return this->origin;
-  }
-
-  Hitbox *Actor::getBox () {
-    return this->box;
-  }
-
-  void Actor::setMoveSpeed (float moveSpeed) {
-    this->moveSpeed = moveSpeed;
-
-    return;
-  }
-
-  void Actor::changeMoveSpeed (float speedChange) {
-    this->moveSpeed += speedChange;
-    
-    return;
   }
 
   void Actor::setOrigin (Coordinates<float> *origin) {
     this->origin = origin;
 
-    return;
-  }
-
-  void Actor::setBox (Hitbox *box) {
-    this->box = box;
-
-    return;
-  }
-
-  void Actor::moveUp (int dist) {
-    this->origin->setY(this->origin->getY() + dist);
-    this->box->moveUp(dist);
-    
-    return;
-  }
-
-  void Actor::moveDown (int dist) {
-    this->origin->setY(this->origin->getY() - dist);
-    this->box->moveDown(dist);
-    
-    return;
-  }
-
-  void Actor::moveRight (int dist) {
-    this->origin->setX(this->origin->getX() + dist);
-    this->box->moveRight(dist);
-        
-    return;
-  }
-
-  void Actor::moveLeft (int dist) {
-    this->origin->setX(this->origin->getX() - dist);
-    this->box->moveLeft(dist);
-    
     return;
   }
 
--- Actor.h
+++ Actor.h
@@ -2,11 +2,8 @@
 #define __ACTOR_H__
 
 #include "Coordinates.h"
-#include "Hitbox.h"
 
 namespace evolver {
-
-  const int ACTOR_MOVE_SPEED = 12;
 
   class Actor {
   public:
@@ -15,24 +12,12 @@ namespace evolver {
     Actor (const Actor &original);
     Actor &operator= (const Actor &original);
 
-    float getMoveSpeed (void);
     Coordinates<float> *getOrigin (void);
-    Hitbox *getBox (void);
 
-    void setMoveSpeed (float moveSpeed);
-    void changeMoveSpeed (float speedChange);
     void setOrigin (Coordinates<float> *origin);
-    void setBox (Hitbox *box);
-
-    void moveUp (int dist);
-    void moveDown (int dist);
-    void moveRight (int dist);
-    void moveLeft (int dist);
 
   private:
-    float moveSpeed;
     Coordinates<float> *origin;
-    Hitbox *box;
   };
 
 }
