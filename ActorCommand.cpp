b798c2989f0bfcca1ff1d46945b6cf993fbde48f
Chris Frisz <cjfrisz@chrisfrisz.com>
2011-09-30 23:30:53 -0400

    Correcting implementation for setActor in ActorCommand to take a reference to an Actor instead of a pointer.
    This was missed in the original conversion and now fixes compilation
    errors.

--- ActorCommand.cpp
+++ ActorCommand.cpp
@@ -29,7 +29,7 @@ namespace evolver {
     return this->actor;
   }
 
-  void ActorCommand::setActor (Actor *actor) {
+  void ActorCommand::setActor (Actor &actor) {
     this->actor = actor;
 
     return;
